fd563a201daeab78ee888a8f2b036e3b076127d4
krigla3 <david.maric4@gmail.com>
2021-10-10 19:34:46 +0000

    Initial commit with short game description

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+    Battleship game
+    
+    - the game will be made on a 10x10 grid area.
+    - the number of battleships per player will be 4.
+    - each batteship can be placed verticaly or horizontaly, no diagonal placements.
+    - the player will be able to select a column and row where the desired shoot will impact.
+    - the number of shots will be 30 or potentially more
+    - the game is one once all 4 ships of the enemy player have been destroyed.
+"""
